403aa71c6004df61fb47660b55deadcb81a089c5
James Greiner <jamesdgreiner@icloud.com>
2023-04-10 18:06:20 -0700

    Initial commit with project scaffolding

---
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/SwiftfulThinking_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinking_CryptoApp.swift
+//  SwiftfulThinking-Crypto
+//
+//  Created by James Greiner on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinking_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
