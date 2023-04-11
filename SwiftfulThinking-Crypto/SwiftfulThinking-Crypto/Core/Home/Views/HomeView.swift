27dde509707ec8f28cc63e3d568342bdfde903d0
James Greiner <jamesdgreiner@icloud.com>
2023-04-10 23:27:38 -0700

    Add HomeView and display as main view

---
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,63 @@
+//
+//  HomeView.swift
+//  SwiftfulThinking-Crypto
+//
+//  Created by James Greiner on 4/10/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // Background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content Layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
