c46e5d611efa9b2aa06d9b797517460d052bb40e
James Greiner <jamesdgreiner@icloud.com>
2023-04-10 22:04:19 -0700

    Add extension to Color to set color scheme for app

---
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  SwiftfulThinking-Crypto
+//
+//  Created by James Greiner on 4/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
--- SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/SwiftfulThinking_CryptoApp.swift
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/SwiftfulThinking_CryptoApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct SwiftfulThinking_CryptoApp: App {
+struct SwiftfulThinkingCryptoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
