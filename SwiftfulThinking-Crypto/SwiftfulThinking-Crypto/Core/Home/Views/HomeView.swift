3f10269d57ab3cf24ba99c66c88e3c6b833acead
James Greiner <jamesdgreiner@icloud.com>
2023-04-11 21:10:06 -0700

    Update HomeView to include coin lists for all coins and portfolio

--- SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Home/Views/HomeView.swift
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Home/Views/HomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject private var vm: HomeViewModel
+    
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -19,6 +21,14 @@ struct HomeView: View {
             // Content Layer
             VStack {
                 homeHeader
+                if !showPortfolio {
+                    allCoinsList
+                    .transition(.move(edge: .leading))
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
                 
                 Spacer(minLength: 0)
             }
@@ -32,6 +42,7 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+        .environmentObject(dev.homeVM)
     }
 }
 
@@ -60,4 +71,26 @@ extension HomeView {
         }
         .padding(.horizontal)
     }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
 }
+
+
