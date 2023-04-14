ebda7a94865e3f67b4997bb18efde4203f23b4b7
James Greiner <jamesdgreiner@icloud.com>
2023-04-13 22:08:56 -0700

    Update Home view model for test statistics data

--- SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,6 +9,12 @@ import Combine
 import Foundation
 
 class HomeViewModel: ObservableObject {
+    @Published var statistics: [StatisticModel] = [
+        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
+    ]
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
