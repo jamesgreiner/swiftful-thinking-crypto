9a3c2ce9259ae78be01dc8b5a3bec964cb518060
James Greiner <jamesdgreiner@icloud.com>
2023-04-14 22:08:46 -0700

    Add view for dismiss button

---
+++ SwiftfulThinking-Crypto/SwiftfulThinking-Crypto/Core/Components/XMarkButton.swift
@@ -0,0 +1,21 @@
+//
+//  XMarkButton.swift
+//  SwiftfulThinking-Crypto
+//
+//  Created by James Greiner on 4/13/23.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.headline)
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
